c4c13bda8f0e1cde76052cbaf5929cbacd007491
elcan101 <eljankha-apa103@code.edu.az>
2026-08-01 01:20:40 +0400

    fproject

--- E-Commerce/E-Commerce/E-Commerce/obj/Debug/net10.0/E-Commerce.AssemblyInfo.cs
+++ E-Commerce/E-Commerce/E-Commerce/obj/Debug/net10.0/E-Commerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df8c5c8615a58d457adcdcf508dcdeb67788aea9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
