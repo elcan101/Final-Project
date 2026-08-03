b4281fe0a11724fff2e5132c5b1ee5a8be4cf530
elcan101 <eljankha-apa103@code.edu.az>
2026-08-04 01:23:41 +0400

    fproject

--- E-Commerce/E-Commerce/Models/AppUser.cs
+++ E-Commerce/E-Commerce/Models/AppUser.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
 namespace E_Commerce.Models
-{
+
     // ASP.NET Core Identity-nin standart istifadəçisini genişləndiririk
     public class AppUser : IdentityUser
-    {
+    
         public string FullName { get; set; } = null!;
    
